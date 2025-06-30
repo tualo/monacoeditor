9187c1c87f8844241049b3a719e6e82b3d5bc79a
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-06-30 11:26:32 +0200

    add serveSecureStaticFile

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -3,6 +3,7 @@
 namespace Tualo\Office\MonacoEditor\Routes;
 
 use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\Basic\RouteSecurityHelper;
 use Tualo\Office\Basic\Route as R;
 use Tualo\Office\Basic\IRoute;
 
@@ -13,33 +14,23 @@ class Route implements IRoute
     {
 
 
-        R::add('/monacoeditorlib/(?P<file>[\/.\w\d\-]+)', function ($matches) {
-            if (
-                R::checkDoubleDots($matches, 'file', 'Path contains ".."') &&
-                file_exists(dirname(__DIR__, 2) . '/lib/' . $matches['file'] . '')
-            ) {
-                $path_parts = pathinfo(dirname(__DIR__, 2) . '/lib/' . $matches['file'] . '');
-
-                if ($path_parts['extension'] == 'js')   TualoApplication::contenttype('application/javascript');
-                if ($path_parts['extension'] == 'css')   TualoApplication::contenttype('text/css');
-                TualoApplication::etagFile((dirname(__DIR__, 2) . '/lib/' . $matches['file'] . ''));
-            } else {
-                TualoApplication::body("// hm, something is wrong " . $matches['file']);
-            }
+        R::add('/monacoeditorlib/(?P<file>[a-zA-Z0-9\-_\/\.]+)', function ($matches) {
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'],
+                dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'lib',
+                ['js', 'css', 'html', 'svg', 'png', 'woff2', 'woff', 'ttf'],
+                [
+                    'application/javascript',
+                    'application/json',
+                    'text/css',
+                    'text/html',
+                    'image/svg+xml',
+                    'image/png',
+                    'font/woff2',
+                    'font/woff',
+                    'font/ttf'
+                ]
+            );
         },  ['get'], false);
-
-
-        /*
-        R::add('/monacoeditorjs/(?P<file>[\/.\w\d\-]+)(?P<remains>[.]+)', function ($matches) {
-            if (file_exists(dirname(__DIR__, 2) . '/src/js/' . $matches['file'] . '')) {
-                $path_parts = pathinfo(dirname(__DIR__, 2) . '/lib/' . $matches['file'] . '');
-                if ($path_parts['extension'] == 'js')   TualoApplication::contenttype('application/javascript');
-                if ($path_parts['extension'] == 'css')   TualoApplication::contenttype('text/css');
-                TualoApplication::etagFile((dirname(__DIR__, 2) . '/src/js/' . $matches['file'] . ''));
-            } else {
-                TualoApplication::body("// hm, something is wrong " . $matches['file']);
-            }
-        }, array('get', 'post'), false);
-        */
     }
 }
